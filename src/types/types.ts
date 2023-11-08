ea393f89eaccfc6b7e0886a27b4393ad68d2cdd5
kristianvaula <kristvje@stud.ntnu.no>
2023-11-08 18:50:30 +0100

    SudokuBoard Type

--- src/types/types.ts
+++ src/types/types.ts
@@ -4,11 +4,33 @@ export type RootStackParamList = {
   Create: undefined;
 };
 
+export type SudokuBoard = {
+  id: string;
+  values: number[][]; // 2D array of numbers
+  markers: number[][]; // 2D array of numbers
+  difficulty: Difficulty;
+  createdAt: string;
+};
+
 export enum Difficulty {
+  Unknown = 'unknown',
   Easy = 'easy',
   Medium = 'medium',
   Hard = 'hard',
 }
+
+export const getDifficulty = (difficulty: string): Difficulty => {
+  switch (difficulty) {
+    case 'easy':
+      return Difficulty.Easy;
+    case 'medium':
+      return Difficulty.Medium;
+    case 'hard':
+      return Difficulty.Hard;
+    default:
+      return Difficulty.Unknown;
+  }
+};
 
 export enum DrawMode {
   Pencil = 'pencil',
