0e1c7e5dfbc119b58b5165500c0fa6916f776e36
kristianvaula <kristvje@stud.ntnu.no>
2023-11-08 14:10:27 +0100

    DrawMode and Difficulty enums

--- src/types/types.ts
+++ src/types/types.ts
@@ -3,3 +3,15 @@ export type RootStackParamList = {
   Sudoku: undefined;
   Create: undefined;
 };
+
+export enum Difficulty {
+  Easy = 'easy',
+  Medium = 'medium',
+  Hard = 'hard',
+}
+
+export enum DrawMode {
+  Pencil = 'pencil',
+  Erase = 'erase',
+  Marker = 'marker',
+}
